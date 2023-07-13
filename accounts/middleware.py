3c7071b12e6576fce07591a631b9c3ddcb30bbb0
runo hr <runoharris215@gmail.com>
2023-07-13 04:58:18 -0700

    Add docstrings for middleware.py

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -1,17 +1,34 @@
-from rest_framework.authtoken.models import Token
+# accounts/middleware.py
 
+from rest_framework.authtoken.models import Token
 from django.utils import timezone
 from django.http import JsonResponse
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 
 class TokenExpirationMiddleware:
     def __init__(self, get_response):
+        """
+        Initialize the TokenExpirationMiddleware.
+
+        Args:
+            get_response (function): The callable that represents the next middleware or view.
+        """
         self.get_response = get_response
 
     def __call__(self, request):
+        """
+        Process the request and perform token expiration checks.
+
+        If the request contains an Authorization header with a token, this middleware will check if the token has expired.
+        If the token has expired, it will be deleted and an error response will be returned.
+        If the token is still valid, its expiration time will be updated to extend its validity.
+
+        Args:
+            request (HttpRequest): The incoming request.
+
+        Returns:
+            HttpResponse: The response from the view or the error response if the token has expired.
+        """
         if 'Authorization' in request.headers:
             auth_header = request.headers['Authorization']
             token_key = auth_header.split(' ')[1]  # Extract the token from the Authorization header
@@ -19,14 +36,14 @@ class TokenExpirationMiddleware:
                 token = Token.objects.get(key=token_key)
 
                 if token.created < timezone.now() - timezone.timedelta(days=5):
-                    token.delete() # Token has expired, delete it
+                    token.delete()  # Token has expired, delete it
                     response_data = {'message': 'Token has expired. Please log in again.'}
                     return JsonResponse(response_data, status=401)
                 else:
                     # Update token's created time to extend its expiration
                     token.created = timezone.now()
                     token.save()
-            
+
             except Token.DoesNotExist:
                 print('Token.DoesNotExist')
                 response_data = {
